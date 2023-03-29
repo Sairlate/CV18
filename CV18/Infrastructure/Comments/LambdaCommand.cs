cefd222efe2f2fd0218c5d71e3d1b58870b1aef3
kpc <kostroma.kpc@gmail.com>
2023-03-29 09:42:15 +0300

    Добавление в главное окно представления функционала кноки вызода

--- CV18/Infrastructure/Comments/LambdaCommand.cs
+++ CV18/Infrastructure/Comments/LambdaCommand.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using CV18.Infrastructure.Comments.Base;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CV18.Infrastructure.Comments
 {
-    class LambdaCommand
+    class LambdaCommand : Command
     {
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+
+        public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = CanExecute;
+        }
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => _Execute(parameter);
     }
 }
