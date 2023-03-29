25c6cc8c1b43586af93593a72de369c2ef2dcb68
kpc <kostroma.kpc@gmail.com>
2023-03-29 11:27:58 +0300

    запрос к удаленному серверу

--- CV18Condole/Program.cs
+++ CV18Condole/Program.cs
@@ -1,11 +1,19 @@
 ﻿using System;
+using System.Net.Http;
 
 namespace CV18Console
 {
     class Program
     {
+        private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
         static void Main(string[] args)
         {
+            var client = new HttpClient();
+
+            var response = client.GetAsync(data_url).Result;
+            var csv_str = response.Content.ReadAsStringAsync().Result;
+
+
             Console.WriteLine("Hello World!");
         }
     }
